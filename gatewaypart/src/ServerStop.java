83d265da49ab1af0e9f9b4b0d46d143ed21d50b4
DivineBee <vizanttriss@gmail.com>
2021-09-23 15:47:27 +0300

    delete other branches contents

--- gatewaypart/src/ServerStop.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class ServerStop {
-    public static void main(String[] args) throws IOException {
-        for( Server server: ServerMain.serversList){
-            server.stop();
-        }
-    }
-}
